bdeb804b466041ac4c27bc48ffb54636b6992aa0
MayaMK <maya.mallabykay@gmail.com>
2020-07-14 09:39:58 -0500

    Adding tables

--- Data/pull_data_curl.sh
+++ Data/pull_data_curl.sh
@@ -43,6 +43,7 @@ curl -O https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr4/maps/s15/act_dr
 
 echo "Getting the Source maps"
 curl -O https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr4/maps/s15/act_dr4.01_s15_D56_pa1_f150_nohwp_night_3pass_4way_set[0-3]_srcs.fits
+curl -O https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr4/maps/s15/act_dr4.01_s15_D56_pa1_f150_nohwp_night_3pass_4way_coadd_srcs.fits
 
 echo "Getting the inverse variance maps"
 curl -O https://lambda.gsfc.nasa.gov/data/suborbital/ACT/ACT_dr4/maps/s15/act_dr4.01_s15_D56_pa1_f150_nohwp_night_3pass_4way_set[0-3]_ivar.fits
