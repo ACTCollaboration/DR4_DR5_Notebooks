f31f2a08015522fdc4451c1bbccc4b293ae372b0
Zachary Atkins <zatkins2@gmail.com>
2020-08-05 12:14:03 -0400

    add pull data for nb 12

---
+++ Data/pull_data_notebook_12_curl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+curl -O https://phy-act1.princeton.edu/public/zatkins/060420_lensed_best_fits_params.txt
+curl -O https://phy-act1.princeton.edu/public/zatkins/060420_delensed_best_fits_params.txt
+curl -O https://phy-act1.princeton.edu/public/zatkins/060320_param_shift_20x20.txt
+
+
+
+
+
+
+
+
+
+
+
---
+++ Data/pull_data_notebook_12_wget.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget -O 060420_lensed_best_fits_params.txt https://phy-act1.princeton.edu/public/zatkins/060420_lensed_best_fits_params.txt
+wget -O 060420_delensed_best_fits_params.txt https://phy-act1.princeton.edu/public/zatkins/060420_delensed_best_fits_params.txt
+wget -O 060320_param_shift_20x20.txt https://phy-act1.princeton.edu/public/zatkins/060320_param_shift_20x20.txt
+
+
+
+
+
+
+
+
+
